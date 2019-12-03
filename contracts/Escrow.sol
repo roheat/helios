6fed0c74901dbe66b68ec6f18912f17fe2ebd955
roheat <rohithsrivathsav@gmail.com>
2019-12-03 17:40:19 +0530

    Escrow.sol added, openzeppelin integration

---
+++ contracts/Escrow.sol
@@ -0,0 +1,41 @@
+pragma solidity 0.4.24;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+
+/*
+ * @title Escrow
+ * @dev A standalone escrow contract that holds the long and short margins
+ */
+
+contract Escrow is Ownable {
+  using SafeMath for uint256;
+  mapping(address => uint256) public escrowBalance;
+
+  /*
+   * @notice Returns deposit amount of an address
+   * @param _payee the payee address
+   */
+  function depositsOf(address _payee) public view returns uint256 {
+    return escrowBalance[_payee];
+  }
+
+  /*
+   * @notice Deposit amount into escrow
+   * @param _payer the payer address
+   * @param _amount the amount deposited
+   */
+  function deposit(address _payer, uint256 _amount) public payable onlyOwner {
+    escrowBalance[_payer] = escrowBalance[_payer].add(_amount);
+  }
+
+  /*
+   * @notice Withdraw amount from escrow
+   */
+  function withdraw(address _payer, address _payee, uint256 _amount) public onlyOwner returns (bool) {
+    require(_amount <= escrowBalance[payer], "Insufficient funds");
+    _payee.transfer(_amount);
+    escrowBalance[_payer] = escrowBalance[_payer].sub(_amount)
+  }
+
+}
