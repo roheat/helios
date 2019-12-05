d0ef05a14be9bc308947d44e3d6ca8d20df431d2
roheat <rohithsrivathsav@gmail.com>
2019-12-05 21:43:18 +0530

    contracts updated

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.4.24;
+pragma solidity >=0.5.0 <0.6.0;
 
 import "../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
 import "../node_modules/@openzeppelin/contracts/ownership/Ownable.sol";
@@ -12,11 +12,15 @@ contract Escrow is Ownable {
   using SafeMath for uint256;
   mapping(address => uint256) public escrowBalance;
 
+  constructor() payable public {
+    escrowBalance[msg.sender] = msg.value;
+  }
+
   /*
    * @notice Returns deposit amount of an address
    * @param _payee the payee address
    */
-  function depositsOf(address _payee) public view returns uint256 {
+  function depositsOf(address _payee) public view returns (uint256) {
     return escrowBalance[_payee];
   }
 
@@ -32,10 +36,10 @@ contract Escrow is Ownable {
   /*
    * @notice Withdraw amount from escrow
    */
-  function withdraw(address _payer, address _payee, uint256 _amount) public onlyOwner returns (bool) {
-    require(_amount <= escrowBalance[payer], "Insufficient funds");
+  function withdraw(address _payer, address payable _payee, uint256 _amount) public onlyOwner returns (bool) {
+    require(_amount <= escrowBalance[_payer], "Insufficient funds");
     _payee.transfer(_amount);
-    escrowBalance[_payer] = escrowBalance[_payer].sub(_amount)
+    escrowBalance[_payer] = escrowBalance[_payer].sub(_amount);
   }
 
 }
--- contracts/FuturesFactory.sol
+++ contracts/FuturesFactory.sol
@@ -1,51 +1,51 @@
-pragma solidity 0.4.24;
+pragma solidity >=0.5.0 <0.6.0;
 
 import "../node_modules/@openzeppelin/contracts/math/SafeMath.sol";
 import "../node_modules/@openzeppelin/contracts/ownership/Ownable.sol";
 import "./Escrow.sol";
-import "./interfaces/IOracle.sol";
+// import "./interfaces/IOracle.sol";
 
 /*
  * @title FuturesFactory
  * @dev Contract that manges futures trading - long/short position and square off
  */
-
- contract FuturesFactory is Ownable {
+contract FuturesFactory is Ownable {
   using SafeMath for uint256;
   address private escrowAddress;
-  address private futureId;
+  uint256 private futureId;
   address private deployedOracleAddress;
-  uint256 public PRICE_PER_LOT;
+  uint256 private positionAmount;
 
   struct future {
     address long;
     address short;
-    uint265 qty;
+    uint256 qty;
     uint256 price;
     uint256 expiry;
   }
 
   event idEvent(uint256);
-  
+
   mapping(uint256 => future) futuresList; // futureId => future
 
-  constructor(address _oracleAddress) public {
+  constructor(address _oracleAddress) public payable {
     deployedOracleAddress = _oracleAddress;
     escrowAddress = address(new Escrow());
     futureId = 1;
-    PRICE_PER_LOT = 100000000000000000; // 0.1 ETH
+    positionAmount = uint256(msg.value);
   }
 
   function fillOrder(
     address _long,
     address _short,
     uint256 _qty,
-    uint256 _price,
-    uint256 _expiry,
-  ) public returns (uint256) {
-    //TODO: instead of qty, pass amount = (qty x PRICE_PER_LOT)
-    require(_transferToEscrow(_qty, _long), "Failed at long _transferToEscrow");
-    require(_transferToEscrow(_qty, _short), "Failed at short _transferToEscrow");
+    uint256 _price, // 0.1 ETH
+    uint256 _expiry
+  ) public payable returns (uint256) {
+    // uint256 amount = _price.mul(_qty);
+    require(positionAmount == msg.value, "Insuficient position amount");
+    require(_transferToEscrow(uint256(positionAmount), _long), "Failed at long _transferToEscrow");
+    require(_transferToEscrow(uint256(positionAmount), _short), "Failed at short _transferToEscrow");
 
     future memory newFuture = future(
       _long,
@@ -60,8 +60,33 @@ import "./interfaces/IOracle.sol";
     futureId = futureId.add(1);
   }
 
-  function _transferToEscrow(uint256 _amount, address _payer) internal returns (bool){
-    Escrow(escrowAddress).deposit(_payer, _amount);
+  function squareOffOrder(uint256 _futureId) public {
+    
+    require(_canBeExercised(_futureId), "Future Contract cannot be squared off");
+  }
+
+  function _canBeExercised(uint256 _futureId) internal view returns (bool) {
+    future memory existingFuture = futuresList[_futureId];
+    require(
+      msg.sender == existingFuture.long || msg.sender == existingFuture.short,
+      "Only long or short seller can exercise option"
+    );
+    require(_futureId < futureId && _futureId > 0, "Invalid Contract ID");
+    // require(now < existingFuture.expiry, "Future Contract has expired");
+    return true;
+  }
+
+  function getEscrowAddress() public view returns (address) {
+    return escrowAddress;
+  }
+
+  function _transferToEscrow(uint256 _amount, address _payer) internal returns (bool) {
+    Escrow(escrowAddress).deposit.value(_amount)(_payer, _amount);
+    return true;
+  }
+
+  function _withdrawFromEscrow(uint256 _amount, address _payer) internal returns (bool) {
+    Escrow(escrowAddress).deposit.value(_amount)(_payer, _amount);
     return true;
   }
 }
